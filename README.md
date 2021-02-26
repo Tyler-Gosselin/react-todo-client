bed58306968488794e6e0c169df5b8eb64df03e7
Tyler <gosselin.code@gmail.com>
2021-02-26 15:46:09 -0700

    Update App to class component

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Todo React Client
+
+## Components needed
+
+-  Todo-Form
+  - form 
+    - input 
+    - submit button
+- Todo Item
+  - check box
+  - title div
+  - font awesome icon
+
+
+## State Values
+
+- title or todo -> string
+- done -> boolean
+- todos -> array
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
